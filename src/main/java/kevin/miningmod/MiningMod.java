c5222532bedc6057cc8209ec999473fd09828106
kdu2030 <kdu2030@gmail.com>
2023-11-12 17:36:40 -0500

    Setup

--- src/main/java/kevin/miningmod/MiningMod.java
+++ src/main/java/kevin/miningmod/MiningMod.java
@@ -5,11 +5,12 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Miningmod implements ModInitializer {
+public class MiningMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("miningmod");
+	public static final String MOD_ID = "miningmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
