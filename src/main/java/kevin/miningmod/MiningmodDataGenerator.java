ff8a7befe112d7ba48183606897e98c2ba8fc53a
kdu2030 <kdu2030@gmail.com>
2023-11-12 16:59:42 -0500

    Initial commit

---
+++ src/main/java/kevin/miningmod/MiningmodDataGenerator.java
@@ -0,0 +1,11 @@
+package kevin.miningmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MiningmodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
