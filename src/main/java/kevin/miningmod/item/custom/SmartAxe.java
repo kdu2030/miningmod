e95b9f545d34cc492df8b2364430937c50b81fce
kdu2030 <kdu2030@gmail.com>
2023-11-13 21:35:34 -0500

    Find Methods Called after block break

--- src/main/java/kevin/miningmod/item/custom/SmartAxe.java
+++ src/main/java/kevin/miningmod/item/custom/SmartAxe.java
@@ -1,12 +1,39 @@
 package kevin.miningmod.item.custom;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.AxeItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class SmartAxe extends AxeItem {
     public SmartAxe(ToolMaterial material, float attackDamage, float attackSpeed, Settings settings) {
         super(material, attackDamage, attackSpeed, settings);
     }
 
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        PlayerEntity player = context.getPlayer();
+        if(player != null){
+            player.sendMessage(Text.literal("This is being used"));
+        }
+        return super.useOnBlock(context);
+    }
 
+    @Override
+    public boolean postMine(ItemStack stack, World world, BlockState state, BlockPos pos, LivingEntity miner) {
+        // NOTE - This is called after a block is mined
+        PlayerEntity player;
+        if(miner instanceof PlayerEntity){
+            player = (PlayerEntity) miner;
+            player.sendMessage(Text.literal("Block cut down"));
+        }
+        return super.postMine(stack, world, state, pos, miner);
+    }
 }
