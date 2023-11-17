12875d7e4f35d4892696250ee7ec54c79a9a2466
kdu2030 <kdu2030@gmail.com>
2023-11-17 07:49:44 -0500

    Fix Max Distance

--- src/main/java/kevin/miningmod/events/BlockBrokenEvent.java
+++ src/main/java/kevin/miningmod/events/BlockBrokenEvent.java
@@ -4,7 +4,6 @@ import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3i;
@@ -13,7 +12,7 @@ import net.minecraft.world.World;
 import java.util.*;
 
 public class BlockBrokenEvent {
-    public static final int MAX_DISTANCE = 10;
+    public static final int MAX_DISTANCE = 3;
     public static final Vec3i[] BLOCK_POS_MODS = {new Vec3i(1, 0, 0), new Vec3i(-1, 0, 0), new Vec3i(0, 1, 0),
             new Vec3i(0, -1, 0), new Vec3i(0, 0, 1), new Vec3i(0, 0, -1)};
 
@@ -49,20 +48,18 @@ public class BlockBrokenEvent {
         return blocksFound;
     }
 
-    public static Set<BlockData> findBlocksOfTheSameType(World world, BlockData startingBlock, PlayerEntity player) {
+    public static Set<BlockData> findBlocksOfTheSameType(World world, BlockData startingBlock) {
         Set<BlockData> blocksFound = new HashSet<>();
         blocksFound = findBlocksOfTheSameType(world, startingBlock, startingBlock, -1, blocksFound);
         for (BlockData blockFound : blocksFound) {
-            BlockPos blockFoundPos = blockFound.getBlockPos();
-            String message = String.format("Block found at (%d, %d, %d)", blockFoundPos.getX(), blockFoundPos.getY(), blockFoundPos.getZ());
-            player.sendMessage(Text.literal(message));
+            world.breakBlock(blockFound.getBlockPos(), true);
         }
         return blocksFound;
     }
 
     public static boolean onBlockBroken(World world, PlayerEntity player, BlockPos blockPos, BlockState blockState, BlockEntity block) {
         BlockData startingBlock = new BlockData(blockPos, blockState);
-        findBlocksOfTheSameType(world, startingBlock, player);
+        findBlocksOfTheSameType(world, startingBlock);
         return true;
     }
 
