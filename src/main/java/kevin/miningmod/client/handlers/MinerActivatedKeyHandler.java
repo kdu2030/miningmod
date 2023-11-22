11976be38b7ea49d25e0e064a692cd05642261e2
kdu2030 <kdu2030@gmail.com>
2023-11-22 18:20:41 -0500

    Refactoring

--- src/main/java/kevin/miningmod/client/handlers/MinerActivatedKeyHandler.java
+++ src/main/java/kevin/miningmod/client/handlers/MinerActivatedKeyHandler.java
@@ -1,17 +1,16 @@
 package kevin.miningmod.client.handlers;
 
 import kevin.miningmod.client.ModNetworkingConstants;
-import kevin.miningmod.client.mining.ClientMinerState;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.network.PacketByteBuf;
 
 public class MinerActivatedKeyHandler {
-    private ClientMinerState clientMinerState;
+    private boolean isClientMinerActivated;
 
     public MinerActivatedKeyHandler(){
-        clientMinerState = new ClientMinerState();
+        isClientMinerActivated = false;
     }
 
     public void handleMinerActivateKeyPress(MinecraftClient client){
@@ -22,13 +21,14 @@ public class MinerActivatedKeyHandler {
         PacketByteBuf packetPayload = PacketByteBufs.create();
         packetPayload.writeInt(client.player.getId());
 
-        if(clientMinerState.isClientMinerActivated()){
+        if(isClientMinerActivated){
             // Send a packet to the server with the player network ID activate miner ID
             ClientPlayNetworking.send(ModNetworkingConstants.ACTIVATE_MINER_ID, packetPayload);
-            clientMinerState.setClientMinerActivated(true);
+            isClientMinerActivated = true;
         } else {
             // Send a packet to deactivate the player's miner
             ClientPlayNetworking.send(ModNetworkingConstants.DEACTIVATE_MINER_ID, packetPayload);
+            isClientMinerActivated = false;
         }
     }
 
--- src/main/java/kevin/miningmod/events/ModEvents.java
+++ src/main/java/kevin/miningmod/events/ModEvents.java
@@ -11,21 +11,21 @@ import org.lwjgl.glfw.GLFW;
 public class ModEvents {
     public static final BlockBrokenEventHandler BLOCK_BROKEN_HANDLER = new BlockBrokenEventHandler();
 
-    public static final KeyBinding MINER_ACTIVATED_KEY = KeyBindingHelper.registerKeyBinding(
-            new KeyBinding("key.miningmod.miner_activated",
-                    InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_LEFT_ALT,
-                    "category.mining_mod.block_broken"));
+//    public static final KeyBinding MINER_ACTIVATED_KEY = KeyBindingHelper.registerKeyBinding(
+//            new KeyBinding("key.miningmod.miner_activated",
+//                    InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_LEFT_ALT,
+//                    "category.mining_mod.block_broken"));
 
     public static void registerEventHandlers(){
         PlayerBlockBreakEvents.BEFORE.register((BLOCK_BROKEN_HANDLER::onBlockBroken));
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if(MINER_ACTIVATED_KEY.isPressed() && client.player != null){
-               BLOCK_BROKEN_HANDLER.setMinerActivated(true);
-            }
-            else if(BLOCK_BROKEN_HANDLER.isMinerActivated()){
-                BLOCK_BROKEN_HANDLER.setMinerActivated(false);
-            }
-        });
+//        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+//            if(MINER_ACTIVATED_KEY.isPressed() && client.player != null){
+//               BLOCK_BROKEN_HANDLER.setMinerActivated(true);
+//            }
+//            else if(BLOCK_BROKEN_HANDLER.isMinerActivated()){
+//                BLOCK_BROKEN_HANDLER.setMinerActivated(false);
+//            }
+//        });
     }
 
 }
