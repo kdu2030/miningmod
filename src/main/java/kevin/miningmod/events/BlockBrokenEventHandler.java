767138371ec5335197550b446cdc8d2e35afaca8
kdu2030 <kdu2030@gmail.com>
2023-11-22 21:13:45 -0500

    Remove Comments

--- src/main/java/kevin/miningmod/events/BlockBrokenEventHandler.java
+++ src/main/java/kevin/miningmod/events/BlockBrokenEventHandler.java
@@ -1,13 +1,10 @@
 package kevin.miningmod.events;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.EntityStatuses;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
@@ -22,7 +19,7 @@ public class BlockBrokenEventHandler {
     public static final Vec3i[] BLOCK_POS_MODS = {new Vec3i(1, 0, 0), new Vec3i(-1, 0, 0), new Vec3i(0, 1, 0),
             new Vec3i(0, -1, 0), new Vec3i(0, 0, 1), new Vec3i(0, 0, -1)};
 
-    public BlockBrokenEventHandler(){
+    public BlockBrokenEventHandler() {
         playersWithActivatedMiners = new HashSet<>();
     }
 
@@ -31,7 +28,7 @@ public class BlockBrokenEventHandler {
         return MathHelper.sqrt(radicand);
     }
 
-    private static boolean areComplementaryDirections(int directionAIndex, int directionBIndex){
+    private static boolean areComplementaryDirections(int directionAIndex, int directionBIndex) {
         return BLOCK_POS_MODS[directionAIndex].multiply(-1).compareTo(BLOCK_POS_MODS[directionBIndex]) == 0;
     }
 
@@ -43,7 +40,7 @@ public class BlockBrokenEventHandler {
         }
 
         for (int i = 0; i < BLOCK_POS_MODS.length; i++) {
-            if (directionFromPreviousBlock >= 0 &&  areComplementaryDirections(directionFromPreviousBlock, i)) {
+            if (directionFromPreviousBlock >= 0 && areComplementaryDirections(directionFromPreviousBlock, i)) {
                 continue;
             }
             BlockPos potentialBlockPos = currentBlockPos.add(BLOCK_POS_MODS[i]);
@@ -58,16 +55,16 @@ public class BlockBrokenEventHandler {
         return blocksFound;
     }
 
-    public void decreaseDurability(PlayerEntity player, Block blockBroken, int numBlocksBroken){
+    public void decreaseDurability(PlayerEntity player, int numBlocksBroken) {
         Hand hand = player.getActiveHand();
         ItemStack activeItemStack = player.getStackInHand(hand);
-        int damageAmount = (int) (blockBroken.getHardness() * numBlocksBroken);
-        int originalDurability  = activeItemStack.getDamage();
-        if(activeItemStack.isDamageable()){
-            if(originalDurability - damageAmount <= 0){
-                player.handleStatus(hand == Hand.MAIN_HAND ? EntityStatuses.BREAK_MAINHAND : EntityStatuses.BREAK_OFFHAND);
-            }
-            activeItemStack.damage(damageAmount, player, (p) -> {});
+        int originalDurability = activeItemStack.getDamage();
+        if (activeItemStack.isDamageable()) {
+            activeItemStack.damage(numBlocksBroken, player, (p) -> {
+                if (originalDurability - numBlocksBroken <= 0) {
+                    player.handleStatus(hand == Hand.MAIN_HAND ? EntityStatuses.BREAK_MAINHAND : EntityStatuses.BREAK_OFFHAND);
+                }
+            });
         }
     }
 
@@ -75,38 +72,36 @@ public class BlockBrokenEventHandler {
         Set<BlockData> blocksFound = new HashSet<>();
         blocksFound = findBlocksOfTheSameType(world, startingBlock, startingBlock, -1, blocksFound);
 
-        if(blocksFound.size() == 0){
+        if (blocksFound.size() == 0) {
             return;
         }
 
         BlockData[] blocksFoundArr = new BlockData[blocksFound.size()];
         blocksFoundArr = blocksFound.toArray(blocksFoundArr);
-        Block blockType = blocksFoundArr[0].getBlockState().getBlock();
 
         for (BlockData blockFound : blocksFoundArr) {
             world.breakBlock(blockFound.getBlockPos(), !player.isCreative());
         }
 
-        if(!player.isCreative()){
-            decreaseDurability(player, blockType, blocksFoundArr.length);
+        if (!player.isCreative()) {
+            decreaseDurability(player, blocksFoundArr.length);
         }
     }
 
     public boolean onBlockBroken(World world, PlayerEntity player, BlockPos blockPos, BlockState blockState, BlockEntity block) {
         // This runs on the server
         if (playersWithActivatedMiners.contains(player)) {
-            world.playSound(player, blockPos, SoundEvents.ENTITY_ITEM_BREAK, SoundCategory.PLAYERS);
             BlockData startingBlock = new BlockData(blockPos, blockState);
             findBlocksOfTheSameType(world, startingBlock, player);
         }
         return true;
     }
 
-    public void activatePlayerMiner(PlayerEntity player){
+    public void activatePlayerMiner(PlayerEntity player) {
         playersWithActivatedMiners.add(player);
     }
 
-    public void deactivatePlayerMiner(PlayerEntity player){
+    public void deactivatePlayerMiner(PlayerEntity player) {
         playersWithActivatedMiners.remove(player);
     }
 
--- src/main/java/kevin/miningmod/events/ModEvents.java
+++ src/main/java/kevin/miningmod/events/ModEvents.java
@@ -1,31 +1,11 @@
 package kevin.miningmod.events;
-
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
-import org.lwjgl.glfw.GLFW;
 
 public class ModEvents {
     public static final BlockBrokenEventHandler BLOCK_BROKEN_HANDLER = new BlockBrokenEventHandler();
 
-//    public static final KeyBinding MINER_ACTIVATED_KEY = KeyBindingHelper.registerKeyBinding(
-//            new KeyBinding("key.miningmod.miner_activated",
-//                    InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_LEFT_ALT,
-//                    "category.mining_mod.block_broken"));
-
     public static void registerEventHandlers(){
         PlayerBlockBreakEvents.BEFORE.register((BLOCK_BROKEN_HANDLER::onBlockBroken));
-//        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-//            if(MINER_ACTIVATED_KEY.isPressed() && client.player != null){
-//               BLOCK_BROKEN_HANDLER.setMinerActivated(true);
-//            }
-//            else if(BLOCK_BROKEN_HANDLER.isMinerActivated()){
-//                BLOCK_BROKEN_HANDLER.setMinerActivated(false);
-//            }
-//        });
     }
 
 }
